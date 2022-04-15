88fe8826994f61fcb97b40dc2f2269f5b5e7f26f
RayuDW <51034418+RayuDW@users.noreply.github.com>
2022-04-15 18:52:42 +0200

    Added cm to inch unit conversion

---
+++ SillyLittleProjects/UnitConverter.Console/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Title = "UnitConverter.Console | by RayuDW";
+
+Console.WriteLine("Convert what?");
+Console.WriteLine("(1) Lenght - cm in inches");
+Console.WriteLine(" * placeholder *");
+Console.WriteLine(" * placeholder *");
+Console.WriteLine(" * placeholder *");
+Console.Write("> ");
+
+bool validInput = int.TryParse(Console.ReadLine(), out int result);
+
+if (result == 1)
+{
+        Console.WriteLine("How many cm?");
+        Console.Write("> ");
+        bool validDoubleInput = double.TryParse(Console.ReadLine(), out double cmToConvert);
+
+        if (validDoubleInput)
+        {
+                double convertedInchValue = cmToConvert / 2.54;
+                Console.WriteLine($"That are {convertedInchValue} inch!");
+        }
+}
+
+else
+{
+        Console.WriteLine("Error!");
+}
